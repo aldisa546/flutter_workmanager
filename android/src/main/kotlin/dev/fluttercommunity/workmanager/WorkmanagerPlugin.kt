bb1c979f8b8c77543e834982f934cd749df9e7dd
aldisa546 <aldisa546@gmail.com>
2025-05-06 14:23:34 +0700

    refactor: Remove unused PluginRegistry import from WorkmanagerPlugin.kt

--- android/src/main/kotlin/dev/fluttercommunity/workmanager/WorkmanagerPlugin.kt
+++ android/src/main/kotlin/dev/fluttercommunity/workmanager/WorkmanagerPlugin.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry
 
 class WorkmanagerPlugin : FlutterPlugin {
 
